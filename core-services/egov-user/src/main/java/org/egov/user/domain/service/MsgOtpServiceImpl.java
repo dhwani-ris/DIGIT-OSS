8421df5a644248aedcb8994bd4addcefac7250ed
Pragat <pragat.singh@dhwaniris.com>
2023-07-23 19:55:46 +0530

    static url added

--- core-services/egov-user/src/main/java/org/egov/user/domain/service/MsgOtpServiceImpl.java
+++ core-services/egov-user/src/main/java/org/egov/user/domain/service/MsgOtpServiceImpl.java
@@ -10,20 +10,21 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MsgOtpServiceImpl implements MsgOtpService {
 
-    @Value("${sms.provider.send.url}")
-    private String smsSendUrl;
 
-    @Value("${sms.provider.verify.url}")
-    private String smsverifyurl;
+//    @Value("${sms.provider.send.url}")
+    private String smsSendUrl="https://api.msg91.com/api/v5/otp";
 
-    @Value("${sms.auth.key}")
-    private String secretKey;
+//    @Value("${sms.provider.verify.url}")
+    private String smsverifyurl="https://control.msg91.com/api/v5/otp/verify";
 
-    @Value("${sms.auth.template.id}")
-    private String templateId;
+//    @Value("${sms.auth.key}")
+    private String secretKey="180577AHgwvM5ZNxN59ef2bf8";
 
-    @Value("${sms.senderid}")
-    private String senderId;
+//    @Value("${sms.auth.template.id}")
+    private String templateId="6241664b1def1c059c532112";
+
+//    @Value("${sms.senderid}")
+    private String senderId="DHWRIS";
 
     public void sendOTP(String phoneNumber) {
         String url = smsSendUrl + "?template_id=" + templateId + "&authkey=" + secretKey + "&mobile=91" + phoneNumber;
